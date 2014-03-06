86e1a94324996727884004f4cf6c99942c0869bd
Michael Conlen <mike@conlen.org>
2014-03-05 20:02:05 -0500

    remove timing

--- src/vektor/test.cpp
+++ src/vektor/test.cpp
@@ -17,13 +17,9 @@ int main(int argc, char *argv[])
 
 
 	cout << "main start" << endl;
-	if((rc = getrusage(RUSAGE_SELF, &ru)) != 0) { perror("getrusage 1");}
-	startTime = ru.ru_utime.tv_sec + ru.ru_stime.tv_sec + ((long double)(ru.ru_utime.tv_usec + ru.ru_stime.tv_sec)) / 1000000;
 
 	test1();
 
-	if((rc = getrusage(RUSAGE_SELF, &ru)) != 0) { perror("getrusage 1");}
-	endTime = ru.ru_utime.tv_sec + ru.ru_stime.tv_sec + ((long double)(ru.ru_utime.tv_usec + ru.ru_stime.tv_sec)) / 1000000;
-	cout << "Computed " << r << " in " << endTime - startTime << endl;
+
 	return(0);
 }
